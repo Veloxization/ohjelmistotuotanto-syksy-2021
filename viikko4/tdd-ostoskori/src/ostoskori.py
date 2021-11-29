25a95fb67d9cc1aedae61e043c6e4d670528b0a9
Iiris H <git@velobun.xyz>
2021-11-29 18:33:29 +0200

    Week 4 exercise 5 step 11 complete

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -7,7 +7,10 @@ class Ostoskori:
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return len(self.ostoslista)
+        tavara_summa = 0
+        for ostos in self.ostoslista:
+            tavara_summa += ostos.lukumaara()
+        return tavara_summa
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
@@ -21,6 +24,10 @@ class Ostoskori:
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
+        for ostos in self.ostoslista:
+            if ostos.tuote == lisattava:
+                ostos.muuta_lukumaaraa(1)
+                return
         self.ostoslista.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
