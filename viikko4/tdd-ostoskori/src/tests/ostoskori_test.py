c55017db1eb1f343400af43f018c2e5ae28dca6e
Iiris H <git@velobun.xyz>
2021-11-29 18:19:31 +0200

    Week 4 exercise 5 step 10 complete

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -71,3 +71,13 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    # step 10
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_kaksi_ostosta(self):
+        maito = Tuote("Maito", 3)
+        mehu = Tuote("Mehu", 4)
+
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(mehu)
+
+        self.assertEqual(len(self.kori.ostokset()), 2)
